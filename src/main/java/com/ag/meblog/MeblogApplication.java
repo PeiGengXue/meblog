9a7a55b4c5d63681948d1cd8bf604c8004c477d4
PeiGengXue <995243163@qq.com>
2020-03-17 14:24:33 +0800

    create project

---
+++ src/main/java/com/ag/meblog/MeblogApplication.java
@@ -0,0 +1,13 @@
+package com.ag.meblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeblogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MeblogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ag/meblog/MeblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ag.meblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeblogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
